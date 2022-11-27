407033907f7b24b4d4d56e499cf6d36f2cf1b1e6
Dušan Reljić <reljicd@gmail.com>
2022-11-27 22:34:35 +0100

    added python utils

---
+++ python/run_tests_using_docker_compose.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+export CONTAINER_NAME=al_data
+echo -e "\nSet docker container name as ${CONTAINER_NAME}\n"
+
+echo -e "\nStop running Docker containers with container name ${CONTAINER_NAME}...\n"
+docker stop "$(docker ps -a | grep ${CONTAINER_NAME} | awk '{print $1}')"
+
+stop_and_delete_containers () {
+    echo -e "\nStop running Docker containers with container name ${CONTAINER_NAME}...\n"
+    docker stop "$(docker ps -a | grep ${CONTAINER_NAME} | awk '{print $1}')"
+
+    echo -e "\nStop and delete Postgres containers... \n"
+    docker stop "$(docker ps -a | grep postgres | awk '{print $1}')"
+    docker rm "$(docker ps -a | grep postgres | awk '{print $1}')"
+
+    echo -e "\nStop and delete Mongo containers... \n"
+    docker stop "$(docker ps -a | grep mongo | awk '{print $1}')"
+    docker rm "$(docker ps -a | grep mongo | awk '{print $1}')"
+}
+
+stop_and_delete_containers
+
+docker-compose -f docker/docker-compose.yml build
+
+echo -e "\nStart Docker Compose...\n"
+docker-compose -f docker/docker-compose.yml run --rm \
+    --name "${CONTAINER_NAME}" \
+    "${CONTAINER_NAME}" -m pytest /app/tests
+
+stop_and_delete_containers
